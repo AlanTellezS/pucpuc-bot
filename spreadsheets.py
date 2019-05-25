a7b81edd5906f6fc86a69368d5fc5bef83bbf432
AlanTellezS <43708959+AlanTellezS@users.noreply.github.com>
2019-05-25 00:35:12 -0500

    asd

--- spreadsheets.py
+++ spreadsheets.py
@@ -34,6 +34,7 @@ def updateDB1_3():
     if not creds or creds.invalid:
         flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
         creds = tools.run_flow(flow, store)
+        
     service = build('sheets', 'v4', http=creds.authorize(Http()))
 
     # Call the Sheets API
